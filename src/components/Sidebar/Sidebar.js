876a0ddfb24252aeb37b420907f351c8d57be306
aMiT chaUhaN <17717751+bluesuiter@users.noreply.github.com>
2021-10-12 11:59:12 +0530

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -13,11 +13,7 @@ const Sidebar = () => {
     }
 
     return (
-        <Nav className="navbar shadow navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-            <div className="navbar-brand js-scroll-trigger" href="#page-top">
-                <span className="d-block d-lg-none">Amit Chauhan</span>
-                <span className="d-none d-lg-block"><img alt="Amit Chauhan" width="500" height="500" className="img-fluid img-profile shadow rounded-circle mx-auto mb-5" src={require('../../assets/images/1623396096273.jpg')} /></span>
-            </div>
+        <Nav className="navbar shadow navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">           
             <button onClick={() => setShowMenu(!showMenu)} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className={`${getShowMenu()} collapse navbar-collapse`} id="navbarSupportedContent">
                 <ul className="navbar-nav">
