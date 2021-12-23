35790e8b41026c93f64afa2499d806b71f8db51a
aMiT chaUhaN <17717751+bluesuiter@users.noreply.github.com>
2021-12-23 14:36:36 +0530

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -13,7 +13,7 @@ const Sidebar = () => {
     }
 
     return (
-        <Nav className="navbar shadow navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">           
+        <Nav className="navbar shadow navbar-expand-lg navbar-dark bg-primary fixed-top">           
             <button onClick={() => setShowMenu(!showMenu)} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className={`${getShowMenu()} collapse navbar-collapse`} id="navbarSupportedContent">
                 <ul className="navbar-nav">
