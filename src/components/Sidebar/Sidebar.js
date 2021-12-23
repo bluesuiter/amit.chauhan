36eb09cbd39ded163783edf29224d9609ccf243e
aMiT chaUhaN <17717751+bluesuiter@users.noreply.github.com>
2021-12-23 17:39:07 +0530

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,23 +5,25 @@ import { NavLink } from 'react-router-dom';
 import './sidebar.css';
 
 
-const Sidebar = () => {
+const Sidebar = () =>
+{
     const [showMenu, setShowMenu] = useState(0);
 
-    const getShowMenu = () => {
+    const getShowMenu = () =>
+    {
         return showMenu === 1 ? 'show' : '';
     }
 
     return (
-        <Nav className="navbar shadow navbar-expand-lg navbar-dark bg-primary fixed-top">           
-            <button onClick={() => setShowMenu(!showMenu)} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-            <div className={`${getShowMenu()} collapse navbar-collapse`} id="navbarSupportedContent">
-                <ul className="navbar-nav">
-                    <NavLink onClick={() => setShowMenu(!showMenu)} className="shadow nav-link font-weight-bold text-uppercase" to={'about'}>About</NavLink>
-                    <NavLink onClick={() => setShowMenu(!showMenu)} className="shadow nav-link font-weight-bold text-uppercase" to={'experience'}>Experience</NavLink>
-                    <NavLink onClick={() => setShowMenu(!showMenu)} className="shadow nav-link font-weight-bold text-uppercase" to={'education'}>Education</NavLink>
-                    <NavLink onClick={() => setShowMenu(!showMenu)} className="shadow nav-link font-weight-bold text-uppercase" to={'skills'}>Skills</NavLink>
-                    <NavLink onClick={() => setShowMenu(!showMenu)} className="shadow nav-link font-weight-bold text-uppercase" to={'interests'}>Interests</NavLink>
+        <Nav className="navbar shadow navbar-expand-lg navbar-dark bg-primary fixed-top">
+            <button onClick={ () => setShowMenu(!showMenu) } className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+            <div className={ `${getShowMenu()} collapse navbar-collapse` } id="navbarSupportedContent">
+                <ul className="navbar-nav mx-auto">
+                    <NavLink onClick={ () => setShowMenu(!showMenu) } className="shadow nav-link font-weight-bold text-uppercase" to={ 'about' }>About</NavLink>
+                    <NavLink onClick={ () => setShowMenu(!showMenu) } className="shadow nav-link font-weight-bold text-uppercase" to={ 'experience' }>Experience</NavLink>
+                    <NavLink onClick={ () => setShowMenu(!showMenu) } className="shadow nav-link font-weight-bold text-uppercase" to={ 'education' }>Education</NavLink>
+                    <NavLink onClick={ () => setShowMenu(!showMenu) } className="shadow nav-link font-weight-bold text-uppercase" to={ 'skills' }>Skills</NavLink>
+                    <NavLink onClick={ () => setShowMenu(!showMenu) } className="shadow nav-link font-weight-bold text-uppercase" to={ 'interests' }>Interests</NavLink>
                 </ul>
             </div>
         </Nav>
