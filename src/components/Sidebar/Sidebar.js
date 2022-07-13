e30c3608b3f5588e7feb20ecda5f8fbf230fcee8
aMiT chaUhaN <17717751+bluesuiter@users.noreply.github.com>
2022-07-13 18:03:28 +0530

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 import './sidebar.css';
 
@@ -19,9 +19,9 @@ const Sidebar = () =>
             <div class="col-10 text-center" href="#page-top">
                 <img height="110px" alt="Amit Chauhan" class="img-fluid img-profile shadow rounded-circle mx-auto mb-3" src={ require('../../assets/images/1623396096273.jpg') } />
                 <div className="py-1 rounded social-icons">
-                    <NavLink className="social-icon linkedin" target="_blank" to='https://in.linkedin.com/in/chauhanamit0810'><i className="fab fa-linkedin-in"></i></NavLink>
-                    <NavLink className="social-icon github" target="_blank" to='https://github.com/bluesuiter'><i className="fab fa-github"></i></NavLink>
-                    <NavLink className="social-icon twitter" target="_blank" to='https://twitter.com/chauhanamit0810'><i className="fab fa-twitter"></i></NavLink>
+                    <NavLink className="social-icon linkedin" target="_blank" to={{ pahtname: 'https://in.linkedin.com/in/chauhanamit0810' }}><i className="fab fa-linkedin-in"></i></NavLink>
+                    <NavLink className="social-icon github" target="_blank" to={{ pahtname: 'https://github.com/bluesuiter' }}><i className="fab fa-github"></i></NavLink>
+                    <NavLink className="social-icon twitter" target="_blank" to={{ pathname: 'https://twitter.com/chauhanamit0810' }}><i className="fab fa-twitter"></i></NavLink>
                 </div>
             </div>
             <button onClick={ () => setShowMenu(!showMenu) } className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
