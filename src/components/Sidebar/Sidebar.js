a2b9bd1ee2d1b00fe4ae168a10dffb64b2898e80
aMiT chaUhaN <17717751+bluesuiter@users.noreply.github.com>
2022-07-13 17:14:25 +0530

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -15,10 +15,18 @@ const Sidebar = () =>
     }
 
     return (
-        <Nav className="navbar shadow navbar-expand-lg navbar-dark bg-primary fixed-top">
+        <Nav id="sideNav" className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+            <div class="col-10 text-center" href="#page-top">
+                <img height="110px" alt="Amit Chauhan" class="img-fluid img-profile shadow rounded mx-auto mb-3" src={ require('../../assets/images/1623396096273.jpg') } />
+                <div className="py-1 rounded social-icons">
+                    <NavLink className="social-icon" target="_blank" to='https://in.linkedin.com/in/chauhanamit0810'><i className="fab fa-linkedin-in"></i></NavLink>
+                    <NavLink className="social-icon" target="_blank" to='https://github.com/bluesuiter'><i className="fab fa-github"></i></NavLink>
+                    <NavLink className="social-icon" target="_blank" to='https://twitter.com/chauhanamit0810'><i className="fab fa-twitter"></i></NavLink>
+                </div>
+            </div>
             <button onClick={ () => setShowMenu(!showMenu) } className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className={ `${getShowMenu()} collapse navbar-collapse` } id="navbarSupportedContent">
-                <ul className="navbar-nav mx-auto">
+                <ul className="navbar-nav">
                     <NavLink onClick={ () => setShowMenu(!showMenu) } className="shadow nav-link font-weight-bold text-uppercase" to={ 'about' }>About</NavLink>
                     <NavLink onClick={ () => setShowMenu(!showMenu) } className="shadow nav-link font-weight-bold text-uppercase" to={ 'experience' }>Experience</NavLink>
                     <NavLink onClick={ () => setShowMenu(!showMenu) } className="shadow nav-link font-weight-bold text-uppercase" to={ 'education' }>Education</NavLink>
@@ -31,4 +39,3 @@ const Sidebar = () =>
 }
 
 export default Sidebar;
-
