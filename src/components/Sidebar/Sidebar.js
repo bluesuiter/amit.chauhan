9c4b0a2a60035e64b7d316b6345ee77a3d55934b
aMiT chaUhaN <17717751+bluesuiter@users.noreply.github.com>
2022-07-13 17:19:52 +0530

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -19,9 +19,9 @@ const Sidebar = () =>
             <div class="col-10 text-center" href="#page-top">
                 <img height="110px" alt="Amit Chauhan" class="img-fluid img-profile shadow rounded mx-auto mb-3" src={ require('../../assets/images/1623396096273.jpg') } />
                 <div className="py-1 rounded social-icons">
-                    <NavLink className="social-icon" target="_blank" to='https://in.linkedin.com/in/chauhanamit0810'><i className="fab fa-linkedin-in"></i></NavLink>
-                    <NavLink className="social-icon" target="_blank" to='https://github.com/bluesuiter'><i className="fab fa-github"></i></NavLink>
-                    <NavLink className="social-icon" target="_blank" to='https://twitter.com/chauhanamit0810'><i className="fab fa-twitter"></i></NavLink>
+                    <NavLink className="social-icon linkedin" target="_blank" to='https://in.linkedin.com/in/chauhanamit0810'><i className="fab fa-linkedin-in"></i></NavLink>
+                    <NavLink className="social-icon github" target="_blank" to='https://github.com/bluesuiter'><i className="fab fa-github"></i></NavLink>
+                    <NavLink className="social-icon twitter" target="_blank" to='https://twitter.com/chauhanamit0810'><i className="fab fa-twitter"></i></NavLink>
                 </div>
             </div>
             <button onClick={ () => setShowMenu(!showMenu) } className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
